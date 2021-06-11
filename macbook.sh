2d607c64021bc4b28d8343881395188543ee3f93
grindlemire <grindlemire@gmail.com>
2021-06-11 09:52:34 -0600

    add macbook command to fix the capslock key

---
+++ macbook.sh
@@ -0,0 +1,2 @@
+# This makes the capslock key the esape key and the escape key the tilde key
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029},{"HIDKeyboardModifierMappingSrc":0x700000029,"HIDKeyboardModifierMappingDst":0x700000036}]}'
