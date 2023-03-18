e0db79abcc7bbe0a24b88399e7dc31d693d8f9a5
grindlemire <grindlemire@gmail.com>
2023-03-17 21:04:41 -0600

    add ssh-fix command

--- zshrc.sh
+++ zshrc.sh
@@ -101,6 +101,10 @@ clone() {
 	git clone git@github.com:$1
 }
 
+fixssh() {
+	. ensure-ssh-agent
+}
+
 # this will prune git branches that have been merged and allows you to edit them before deleting them
 alias gprune=gitprune
 
